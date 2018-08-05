34d075acd72b81fc0874cddb1b1d1cf566c845a1
Maxim Tkachenko <mtkachenko@ebay.com>
2018-08-05 20:15:08 +0200

    Pause command works.

--- im/myhome/control_center.py
+++ im/myhome/control_center.py
@@ -4,21 +4,42 @@ import json
 import time
 from threading import Thread
 
-from im.myhome.config_local import mqtt_username, mqtt_password, mqtt_port, mqtt_url
+from im.myhome.config import mqtt_username, mqtt_password, mqtt_port, mqtt_url
 
 NAME = "control_center"
 CHANNEL_OUT = "home/out"
 CHANNEL_IN = "home/in"
-COLLECTING_SENSORS_TIME_SEC = 3
+
+CMD_OFF = "off"
+CMD_ON = "on"
+CMD_PAUSE = "pause"
+
+TYPE_MOTION_SENSOR = "motion_sensor"
+
+STATE_OFF = 0
+STATE_OK = 1
+STATE_INIT = 2
+STATE_PAUSED = 5
+
+COLLECTING_SENSORS_INTERVAL_SEC = 15
+PAUSE_INTERVAL_SEC = 15 * 60  # 15 min
+# COLLECTING_SENSORS_INTERVAL_SEC = 4
+# PAUSE_INTERVAL_SEC = 4
 
 is_collecting_sensors = False
+is_pausing = False
+is_resuming = False
 sensors = dict()
+awaiting_sensors = set()
+paused_sensors = set()
+
+subscribe_out_mid = 0
 
 
 def on_connect(client, userdata, flags, rc):
-    log.i("Connected with result code " + str(rc))
-    (_, mid) = client.subscribe("home/out")
-    log.i("Subscribing " + str(mid))
+    global subscribe_out_mid
+    (_, subscribe_out_mid) = client.subscribe("home/out")
+    client.subscribe("home/in")
 
 
 def on_mqtt_message(client, userdata, msg):
@@ -27,7 +48,7 @@ def on_mqtt_message(client, userdata, msg):
 
     try:
         message = json.loads(msg_str)
-        on_message(message)
+        on_message(client, message)
     except json.JSONDecodeError as e:
         error_message = "cannot parse message: " + msg_str + "\n" + str(e)
         log.e(error_message)
@@ -35,47 +56,133 @@ def on_mqtt_message(client, userdata, msg):
 
 
 def on_subscribe(client, userdata, mid, granted_qos):
-    log.i("Subscribed " + str(mid))
-    start_collecting_sensors(client)
+    if mid == subscribe_out_mid:
+        start_collecting_sensors(client)
 
 
-def stop_collecting_sensors():
-    time.sleep(COLLECTING_SENSORS_TIME_SEC)
+def stop_collecting_sensors(client):
+    time.sleep(COLLECTING_SENSORS_INTERVAL_SEC)
     global is_collecting_sensors
     is_collecting_sensors = False
-    log.i("Stopped collecting sensors, got " + str(len(sensors)))
-    for s in sensors:
-        log.i("    " + s)
+
+    sensors_count = len(sensors)
+    state_ok = dict(name=NAME, state=STATE_OK, value=sensors_count)
+    publish_out(client, state_ok)
+
+    log.i("Stopped collecting sensors, got " + str(sensors_count))
 
 
 def start_collecting_sensors(client):
     global is_collecting_sensors
     is_collecting_sensors = True
 
-    cmd = dict(name='all', cmd='state')
-    client.publish(CHANNEL_IN, json.dumps(cmd))
+    state_init = dict(name=NAME, state=STATE_INIT)
+    publish_out(client, state_init)
 
-    # scheduler.enter(10, 1, stop_collecting_sensors)
-    # scheduler.run(blocking=False)
+    state_cmd = dict(name='all', cmd='state')
+    publish_in(client, state_cmd)
 
-    t = Thread(target=stop_collecting_sensors)
+    t = Thread(target=stop_collecting_sensors, args=[client])
     t.start()
 
     log.i("Started collecting sensors")
 
 
-def on_message(message):
-    # log.i(str(message))
-    global sensors
-    global is_collecting_sensors
+def start_pause(client):
+    global awaiting_sensors, is_pausing
+    awaiting_sensors = {s_name for s_name, s_type in sensors.items() if s_type == TYPE_MOTION_SENSOR}
+    log.i("pausing sensors: " + str(awaiting_sensors))
 
-    if is_collecting_sensors:
-        sensors[message['name']] = message['type']
-        log.i("sensors count = " + str(len(sensors)))
+    is_pausing = True
+    for s in awaiting_sensors:
+        off_cmd = dict(name=s, cmd=CMD_OFF)
+        publish_in(client, off_cmd)
+
+
+def on_message(client, message):
+    # log.i(str(message))
+    global sensors, is_collecting_sensors, is_pausing, paused_sensors
+
+    sensor_name = message.get('name', None)
+    sensor_type = message.get('type', None)
+    cmd = message.get('cmd', None)
+    sensor_state = message.get('state', None)
+
+    if sensor_name is None:
+        return
+
+    if is_collecting_sensors and sensor_name is not None and sensor_type is not None:
+        sensors[sensor_name] = sensor_type
+        log.i("+ " + sensor_name + " [" + str(len(sensors)) + "]")
+
+    if sensor_name == NAME and cmd == CMD_PAUSE:
+        start_pause(client)
+
+    if is_pausing and sensor_state == STATE_OFF:
+        log.i("GOT response: " + sensor_name + " is off")
+        if sensor_name in awaiting_sensors:
+            awaiting_sensors.discard(sensor_name)
+            paused_sensors.add(sensor_name)
+
+        if len(awaiting_sensors) == 0:
+            on_all_paused(client)
+
+    if is_resuming and sensor_state == STATE_OK:
+        log.i("GOT response: " + sensor_name + " is on")
+        if sensor_name in awaiting_sensors:
+            awaiting_sensors.discard(sensor_name)
+            paused_sensors.discard(sensor_name)
+
+        if len(awaiting_sensors) == 0:
+            on_all_resumed(client)
+
+
+def on_all_resumed(client):
+    global is_resuming
+    state = dict(name=NAME, state=STATE_OK)
+    publish_out(client, state)
+    is_resuming = False
+
+
+def on_all_paused(client):
+    global is_pausing
+    state = dict(name=NAME, state=STATE_PAUSED, value=PAUSE_INTERVAL_SEC)
+    publish_out(client, state)
+    is_pausing = False
+
+    Thread(target=resume_all_paused, args=[client]).start()
+
+
+def resume_all_paused(client):
+    global is_resuming
+    time.sleep(PAUSE_INTERVAL_SEC)
+
+    is_resuming = True
+    for s_name in paused_sensors:
+        on_cmd = dict(name=s_name, cmd=CMD_ON)
+        publish_in(client, on_cmd)
+        awaiting_sensors.add(s_name)
+
+
+def publish_in(client, message):
+    log.i("> IN " + json.dumps(message))
+    client.publish(CHANNEL_IN, json.dumps(message))
+
+
+def publish_out(client, message):
+    log.i("> OUT " + json.dumps(message))
+    client.publish(CHANNEL_OUT, json.dumps(message))
 
 
 def publish_debug(client, error_message):
     client.publish("home/debug", error_message)
+
+
+# def call_start_pause():
+#     log.i('a, start')
+#     time.sleep(5)
+#     log.i('a, after sleep')
+#     start_pause(mqtt_client)
 
 
 log.init()
@@ -86,4 +193,8 @@ mqtt_client.on_connect = on_connect
 mqtt_client.on_subscribe = on_subscribe
 mqtt_client.on_message = on_mqtt_message
 mqtt_client.connect(mqtt_url, mqtt_port, 60)
+
+# tt = Thread(target=call_start_pause)
+# tt.start()
+
 mqtt_client.loop_forever()
